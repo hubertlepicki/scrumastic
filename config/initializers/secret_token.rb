f23181f5ba20a69084810cbc1f855da9eb1031a2
Hubert <hubert.lepicki@amberbit.com>
2010-08-29 22:58:28 +0200

    app scaffold

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Scrumastic::Application.config.secret_token = 'b2b99b7eca09be6b6fe8d4b5252e8ea56a9d2e8a603dcc78f4762794d07493331e4723edaf37ca93b0ef72112f277a8d63d956843117c6550a7e1d9be7ab12ff'
